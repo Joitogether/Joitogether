416691a7b622e8e3af4fcb0e146362c70aca595a
Jun Wang <change60201@gmail.com>
2024-11-17 22:04:21 +0800

    files: delete useless files and reorganized files

---
+++ src/views/Home/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BannerComponent from './components/BannerComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
+<template>
+  <BannerComponent></BannerComponent>
+  <FooterComponent></FooterComponent>  
+</template>
+<style scoped>
+</style>
