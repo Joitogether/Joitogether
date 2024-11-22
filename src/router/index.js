6b269414cba6cb66e6ef6402695f0342be6fc934
柯宣宇 <yelloway@Yelloways-Mac-Air.local>
2024-11-22 15:47:51 +0800

    layout add PersonRate

--- src/router/index.js
+++ src/router/index.js
@@ -5,45 +5,49 @@ import Home from '@/views/Home/index.vue'
 import PersonInfo from '@/views/MyProfile/component/PersonInfo.vue'
 import PersonActivity from '@/views/MyProfile/component/PersonActivity.vue'
 import PersonPost from '@/views/MyProfile/component/PersonPost.vue'
-
+import PersonRate from '@/views/MyProfile/component/PersonRate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    component: Profile,
-    children: [
-      {
-        path: 'personInfo',
-        name: 'personInfo',
-        component: PersonInfo
-      },
-      {
-        path: 'personpost',
-        name: 'personpost',
-        component: PersonPost,
-      },
-      {
-        path: 'personActivity',
-        name: 'personActivity',
-        component: PersonActivity,
-      },
-    ]
-  },
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  }
-
-],
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile,
+      children: [
+        {
+          path: 'personInfo',
+          name: 'personInfo',
+          component: PersonInfo,
+        },
+        {
+          path: 'personalrate',
+          name: 'personalrate',
+          component: PersonRate,
+        },
+        {
+          path: 'personpost',
+          name: 'personpost',
+          component: PersonPost,
+        },
+        {
+          path: 'personActivity',
+          name: 'personActivity',
+          component: PersonActivity,
+        },
+      ],
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+  ],
 })
 
 export default router
---
+++ src/views/MyProfile/component/PersonRate.vue
@@ -0,0 +1,174 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import { ArrowLeft, Heart, Search, HeartSolid } from '@iconoir/vue'
+import { NProgress, NDropdown, NButton, NRate } from 'naive-ui'
+
+const comment = reactive([
+  {
+    id: 1,
+    userId: 1,
+    userName: '王大錘',
+    location: '台北',
+    content: '今天在這裡玩很開心謝謝大家。',
+    updatedAt: '2024年11月21日',
+    likesCount: 5,
+  },
+  {
+    id: 2,
+    userId: 2,
+    userName: '李小明',
+    location: '台中',
+    content: '環境很好，主辦方很專業。',
+    updatedAt: '2024年11月20日',
+    likesCount: 5,
+  },
+])
+</script>
+
+<template>
+  <div class="mx-6 mt-6">
+    <section class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <div>
+        <div>
+          <div class="text-center">
+            <section>
+              <h2 class="flex justify-center m-2">
+                <div><Heart width="50" height="50" /></div>
+                <div class="text-5xl">0.00</div>
+              </h2>
+              <p></p>
+            </section>
+          </div>
+          <div class="mb-3">
+            <ol>
+              <li class="flex">
+                5
+                <n-progress
+                  type="line"
+                  color="green"
+                  :percentage="60"
+                  indicator-placement="inside"
+                  processing
+                />
+              </li>
+              <li class="flex">
+                4
+                <n-progress
+                  type="line"
+                  color="green"
+                  :percentage="60"
+                  indicator-placement="inside"
+                  processing
+                />
+              </li>
+              <li class="flex">
+                3
+                <n-progress
+                  type="line"
+                  color="green"
+                  :percentage="60"
+                  indicator-placement="inside"
+                  processing
+                />
+              </li>
+              <li class="flex">
+                2
+                <n-progress
+                  type="line"
+                  color="green"
+                  :percentage="60"
+                  indicator-placement="inside"
+                  processing
+                />
+              </li>
+              <li class="flex">
+                1
+                <n-progress
+                  type="line"
+                  color="green"
+                  :percentage="60"
+                  indicator-placement="inside"
+                  processing
+                />
+              </li>
+            </ol>
+          </div>
+          <div class="mb-2">
+            <h3 class="text-lg">評價細項</h3>
+          </div>
+          <div class="">
+            <div class="flex border-b-4">
+              <div><Heart /></div>
+              <div class="flex justify-between w-full">
+                <div>親切度</div>
+                <div>5</div>
+              </div>
+            </div>
+
+            <div class="flex border-b-4">
+              <div><Heart /></div>
+              <div class="flex justify-between w-full">
+                <div>主辦能力</div>
+                <div class="">5</div>
+              </div>
+            </div>
+            <div class="flex border-b-4">
+              <div><Heart /></div>
+              <div class="flex justify-between w-full">
+                <div>信用度</div>
+                <div class="justify-self-end">5</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="border-b-4 mb-5">
+          <div class="flex justify-between mb-4">
+            <div class="m-auto text-xl">？？則評價</div>
+            <div>
+              <n-dropdown trigger="click" :options="options" @select="handleSelect">
+                <n-button>最新</n-button>
+              </n-dropdown>
+            </div>
+          </div>
+          <div class="flex flex-wrap mb-4">
+            <div><Search /></div>
+            <label for="reviews-search-input">
+              <input type="text" id="reviews-search-input" placeholder="搜尋評價"
+            /></label>
+          </div>
+        </div>
+        <div class="mt-2">
+          <div class="mb-4">
+            <div v-for="userPost in comment" :key="userPost.id" class="mb-6">
+              <section class="flex gap-2">
+                <div>
+                  <a href=""><img src="https://fakeimg.pl/48x48/200" class="rounded-full" /></a>
+                </div>
+                <div>
+                  <h2>{{ userPost.userName }}</h2>
+                  <div>{{ userPost.location }}</div>
+                </div>
+              </section>
+              <div class="flex">
+                <div class="flex items-center mb-2">
+                  <n-rate color="red" readonly :default-value="3">
+                    <n-icon size="2x">
+                      <HeartSolid />
+                    </n-icon>
+                  </n-rate>
+                </div>
+                <div>・</div>
+                {{ userPost.updatedAt }}
+              </div>
+              <div>
+                <span>{{ userPost.content }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
