25578c16c3b8ed82af4febe48a30c76e98085056
h-e-l-e-n <leeleilei07@gmail.com>
2024-11-28 15:58:41 +0800

    others: change tags name

--- src/views/MyProfile/component/person.js
+++ src/views/MyProfile/component/person.js
@@ -1,23 +1,22 @@
 import { ref } from 'vue'
 const userInfo = ref({
-  name: 'Jun',
+  nickName: 'Jun',
   city: '新北市',
   age: 26,
-  job: '工程師',
-  photoURL:"https://shoplineimg.com/5ab875018d1db9e3db00025b/5e15c900fe2d2b002db19bfb/800x.jpg?",
-  shortDescription: '當上帝給你檸檬時，把他們榨成汁',
+  career: '工程師',
+  photo_url:"https://shoplineimg.com/5ab875018d1db9e3db00025b/5e15c900fe2d2b002db19bfb/800x.jpg?",
+  favorite_sentence: '當上帝給你檸檬時，把他們榨成汁',
   tags: ['愛運動', '遊山玩水', '聽音樂', '玩遊戲', '睡覺', '寫程式'],
-  selfintroduction: '大家好我喜歡交朋友大家好我喜歡交朋友大家好我喜歡交朋友',
+  introduction: '大家好我喜歡交朋友大家好我喜歡交朋友大家好我喜歡交朋友',
   zodiac: '處女座',
   hobby: '玩電腦遊戲',
   expertise: '打code',
-  interestedin: '吃美食、玩桌遊、追星',
-  lifePhoto1: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZhdlBWBKtVg0iMZMf3Rrgk9w8BHO-RrtyzA&s',
-  lifePhoto2: 'https://shoplineimg.com/5ab875018d1db9e3db00025b/5e15c900fe2d2b002db19bfb/800x.jpg?'
-})
+  interested_in: '吃美食、玩桌遊、追星',
+  life_photo_1: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZhdlBWBKtVg0iMZMf3Rrgk9w8BHO-RrtyzA&s',
+  life_photo_2: 'https://shoplineimg.com/5ab875018d1db9e3db00025b/5e15c900fe2d2b002db19bfb/800x.jpg?',
 
-localStorage.setItem("user", JSON.stringify(userInfo.value));
-console.log(JSON.parse(localStorage.getItem("user")));
+
+})
 
 
 export default userInfo;
