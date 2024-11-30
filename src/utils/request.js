6964ebd4e84272933ac57d003670c1e7f3488a97
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-11-30 11:33:07 +0800

    other:  ✏️ temporarily comment out a section in request.js for smooth execution

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { getIdToken } from 'firebase/auth'
-import { auth } from './firebaseConfig'
+// import { getIdToken } from 'firebase/auth'
+// import { auth } from './firebaseConfig'
 
 const apiAxios = axios.create({
   baseURL: 'http://localhost:3030',
@@ -8,18 +8,18 @@ const apiAxios = axios.create({
   headers: { 'X-Custom-Header': 'foobar' },
 })
 
-apiAxios.interceptors.request.use(
-  async function (config) {
-    const token = await getIdToken(auth.currentUser)
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  function (error) {
-    return Promise.reject(error)
-  },
-)
+// apiAxios.interceptors.request.use(
+//   async function (config) {
+//     const token = await getIdToken(auth.currentUser)
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   function (error) {
+//     return Promise.reject(error)
+//   },
+// )
 
 apiAxios.interceptors.response.use(
   function (response) {
