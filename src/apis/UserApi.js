58a8827596df79c5fa275352c6410e57ca1ad507
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-02 20:14:30 +0800

    feature: update PUT and is running!!!!

--- src/apis/UserApi.js
+++ src/apis/UserApi.js
@@ -15,17 +15,11 @@ export const UserGetApi = async (uid) => {
 
 export const UserPutApi = async(uid, data) => {
   try {
-  const response = await apiAxios.put(`/users/update/${uid}`, data)
-  if (response.data.status === 220) {
-  console.log('更新成功：', response.data);
-  return { success: true, data: response.data };
-} else {
-    console.log('更新失敗：', response.data);
-    return { success: false, error: response.data.message };
-  }
+    const response = await apiAxios.put(`/users/update/${uid}`, data);
+    return response
   } catch (err) {
-    console.error('更新失敗：', err.message);
-    return { success: false, error: err.message };
+    return err.response.data
+
   }
 
 }
