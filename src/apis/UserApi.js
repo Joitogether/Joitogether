8071a776c963f81a456f355be57bd2922041e062
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-07 00:14:27 +0800

    feature: add follow api

--- src/apis/UserApi.js
+++ src/apis/UserApi.js
@@ -1,4 +1,4 @@
-import { apiAxios } from '../utils/request';
+import { apiAxios } from '../utils/request.js';
 
 
 export const UserGetApi = async (uid) => {
@@ -12,6 +12,45 @@ export const UserGetApi = async (uid) => {
     return err.response.message
   }
 };
+
+export const UserGetFollowerApi = async (uid) => {
+  try {
+    const response = await apiAxios.get(`/users/userFollowers/${uid}`);
+
+    if (response && response.status === 200) {
+      return response.data.data
+    }
+  } catch (err) {
+
+    return err.response.message
+
+  }
+};
+
+export const UserGetFollowingApi = async (uid) => {
+  try {
+    const response = await apiAxios.get(`/users/following/${uid}`);
+
+    if (response && response.status === 200) {
+      return response.data.data
+    }
+  } catch (err) {
+    return err.response.message
+  }
+};
+
+export const UserGetActivityApi = async (uid) => {
+  try {
+    const response = await apiAxios.get(`/users/applications/${uid}`);
+
+    if (response && response.status === 200) {
+      return response.data.data
+    }
+  } catch (err) {
+    return err.response.message
+  }
+};
+
 
 export const UserPutApi = async(uid, data) => {
   try {
