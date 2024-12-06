78e6259205dbea516f726c79ba35798fb188dd99
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-07 00:17:16 +0800

    feature: add get follower feature

--- src/views/MyProfile/component/PersonalFocus.vue
+++ src/views/MyProfile/component/PersonalFocus.vue
@@ -1,50 +1,54 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { NTabs, NTabPane, NDivider } from 'naive-ui'
+import { UserGetFollowerApi } from '../../../apis/UserApi'
+import { useUserStore } from '@/stores/userStore';
 
-const followingList = reactive([
-  {
-    userId: 1,
-    imgUrl: 'https://img.lovepik.com/element/40080/7463.png_300.png',
-    userName: '王大錘',
-    think: '我好帥',
-  },
-  {
-    userId: 2,
-    imgUrl: 'https://img.lovepik.com/element/40116/7316.png_300.png',
-    userName: '張大仙',
-    think: '想交朋友',
-  },
-  {
-    userId: 3,
-    imgUrl: 'https://img.lovepik.com/element/40253/7513.png_300.png',
-    userName: '劉大吉',
-    think: '請多多追隨',
-  },
-])
+const follower = ref(null);
+const loading = ref(true);
+const errorMessage = ref(null);
+const userStore = useUserStore()
 
-const followerList = reactive([
-  {
-    userId: 1,
-    imgUrl:
-      'https://img.lovepik.com/free_png/32/25/61/59758PIC7z1fa7i86bqr58PIC_PIC2018.png_300.png',
-    userName: '彭大海',
-    think: '今天很想跟朋友一起出門',
-  },
-  {
-    userId: 2,
-    imgUrl: 'https://img.lovepik.com/photo/45014/5426.jpg_wh300.jpg',
-    userName: '謝小夜',
-    think: '大家好像都玩得很開心',
-  },
-  {
-    userId: 3,
-    imgUrl:
-      'https://img.lovepik.com/png/20231023/Cute-cartoon-pink-hairball-pig-rabbit-monster-tooth-ear_322945_wh300.png',
-    userName: '蔡大雞',
-    think: '跨年要做什麼呢～？',
-  },
-])
+const fetchFollowerData = async () => {
+  try {
+    const result = await UserGetFollowerApi(userStore.user.uid);
+    console.log('follower回傳資料:', result);
+
+    if (result) {
+      follower.value = result;
+      loading.value = false;
+      return follower.value
+    }
+  } catch (err) {
+    errorMessage.value = err.message || '資料加載錯誤';
+    loading.value = false;
+  }
+    }
+
+const fetchFollowingData = async () => {
+  try {
+    const result = await UserGetFollowerApi(userStore.user.uid);
+    console.log('following回傳資料:', result);
+
+    if (result) {
+      follower.value = result;
+      loading.value = false;
+      return follower.value
+    }
+  } catch (err) {
+  errorMessage.value = err.message || '資料加載錯誤';
+  loading.value = false;
+  }
+}
+
+
+onMounted(() => {
+  fetchFollowerData();
+  fetchFollowingData()
+
+})
+
+
 </script>
 
 <template>
@@ -55,17 +59,16 @@ const followerList = reactive([
         <div>
           <div
             class="my-5"
-            v-for="userFollowingList in followingList"
-            :key="userFollowingList.userId"
           >
             <div>
               <a class="flex" href="">
                 <div class="me-5 max-w-[44px] max-h-[44px]">
-                  <img :src="userFollowingList.imgUrl" class="rounded-full self-center" />
+                  <img class="rounded-full self-center" />
                 </div>
                 <div>
-                  <div>{{ userFollowingList.userName }}</div>
-                  <div>{{ userFollowingList.think }}</div>
+                  {{ follower }}
+                  <!-- <div>{{ userFollowingList.userName }}</div> -->
+                  <!-- <div>{{ userFollowingList.think }}</div> -->
                 </div>
               </a>
             </div>
@@ -74,14 +77,14 @@ const followerList = reactive([
       </n-tab-pane>
       <n-tab-pane name="chap2" tab="粉絲">
         <div>
-          <div class="my-5" v-for="userFollowerList in followerList" :key="userFollowerList.userId">
+          <div class="my-5" >
             <div class="flex">
               <div class="me-5 max-w-[44px] max-h-[44px]">
-                <img :src="userFollowerList.imgUrl" class="rounded-full" />
+                <img class="rounded-full" />
               </div>
               <div>
-                <div>{{ userFollowerList.userName }}</div>
-                <div>{{ userFollowerList.think }}</div>
+                <!-- <div>{{ userFollowerList.userName }}</div> -->
+                <!-- <div>{{ userFollowerList.think }}</div> -->
               </div>
             </div>
           </div>
