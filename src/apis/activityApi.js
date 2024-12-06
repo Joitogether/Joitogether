8c27f707294591cbeb52ab86a58fd48fb3d04d8a
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-07 00:34:30 +0800

    other: first push api

---
+++ src/apis/activityApi.js
@@ -0,0 +1,65 @@
+import { apiAxios } from "@/utils/request";
+
+export const activityCancelRegisterAPI = async (activityId, participant_id) => {
+  try{
+    return await apiAxios.put(`/applications/cancel/${activityId}`, { participant_id })
+  }catch(err){
+    return err.response.data
+  }
+}
+
+export const activityRegisterAPI = async (activity_id ,data) => {
+  try{
+    return await apiAxios.post(`/applications/register/${activity_id}`, data)
+  }catch(err){
+    return err.response.data
+  }
+}
+
+export const activityGetDetailAPI = async (activityId) => {
+  try{
+    const response = await apiAxios.get(`/activities/${activityId}`)
+    if(response && response.status === 200) {
+        return response.data.data
+      }
+    }catch(err){
+      //沒抓到
+      return null
+    }
+}
+
+export const activityGetAPI = async () => {
+  try{
+    const response = await apiAxios.get(`/activities`)
+    if(response && response.status === 200) {
+        return response.data.data
+      }
+    }catch(err){
+      //沒抓到
+      return null
+    }
+}
+
+export const activityCancelAPI = async (activityId) => {
+  try {
+    return await apiAxios.put(`/activities/cancel/${activityId}`)
+  } catch (err) {
+    return err.response.data
+  }
+}
+
+export const activityNewCommentAPI = async (activity_id, data) => {
+  try{
+    return await apiAxios.post(`/activities/comment/${activity_id}`, data)
+  }catch (err) {
+    return err.response.data
+  }
+}
+
+export const activityDeleteCommentAPI = async (comment_id) => {
+  try{
+    return await apiAxios.delete(`/activities/comment/${comment_id}`)
+  }catch (err) {
+    return err.response.data
+  }
+}
