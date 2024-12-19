9448d69107ecce8bab38a1bf3f3cf82f21445c16
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-20 00:08:08 +0800

    feat: implement cart data fetching and render items on the checkout page

---
+++ src/apis/checkoutAPI.js
@@ -0,0 +1,13 @@
+import { apiAxios } from '@/utils/request.js'
+
+export const getCartItemsAPI = async (uid) => {
+  const response = await apiAxios.get(`/carts/${uid}`)
+  console.log('購物車資料獲取成功', response.data)
+  return response.data
+}
+
+export const createOrderAPI = async (orderData) => {
+  const response = await apiAxios.post('/orders', orderData)
+  console.log('訂單資料傳送成功', response.data)
+  return response.data
+}
--- src/views/Payment/CheckoutPage.vue
+++ src/views/Payment/CheckoutPage.vue
@@ -1,39 +1,59 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import { useUserStore } from '@/stores/userStore.js'
+import * as CheckoutAPIs from '../../apis/checkoutAPI.js'
+
+const userStore = useUserStore()
+
+const cartItems = ref([])
+const subtotal = ref(0)
+const fetchCartItems = async () => {
+  try {
+    const response = await CheckoutAPIs.getCartItemsAPI(userStore.user.uid)
+
+    cartItems.value = response.data.cartItems.map((item) => ({
+      title: item.activities.name,
+      photo: item.activities.img_url,
+      price: Number(item.activities.price),
+    }))
+
+    subtotal.value = cartItems.value.reduce((total, item) => total + item.price, 0)
+    console.log(subtotal.value)
+  } catch (error) {
+    console.error('購物車資料獲取失敗:', error)
+  }
+}
+
+const total = computed(() => {
+  return subtotal.value
+})
+
+onMounted(() => {
+  fetchCartItems()
+})
+</script>
+
 <template>
   <div class="w-full min-h-screen bg-gray-200 flex flex-col gap-4 py-14 px-4 md:px-10 lg:px-40">
     <div class="flex flex-col gap-4">
       <p class="text-center text-neutral-800 text-2xl">購物車明細</p>
       <div class="space-y-4">
-        <div class="flex flex-col justify-between px-4 py-4 bg-gray-100 rounded-md md:px-10">
+        <div
+          v-for="(item, index) in cartItems"
+          :key="index"
+          class="flex flex-col justify-between px-4 py-4 bg-gray-100 rounded-md md:px-10"
+        >
           <div class="flex flex-row gap-3 md:gap-5">
-            <div class="w-28 h-28 bg-gray-300 rounded-md shrink-0">
-              <img src="" alt="" />
+            <div class="w-28 h-28 bg-gray-300 rounded-md shrink-0 overflow-hidden">
+              <img :src="item.photo" alt="活動圖片" />
             </div>
             <div class="w-2/3 md:w-full flex flex-col justify-between">
               <div class="">
-                <h3 class="text-neutral-950">活動名稱</h3>
-                <p class="text-sm text-neutral-800">
-                  活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容
-                </p>
+                <h3 class="text-neutral-950">{{ item.title }}</h3>
+                <!-- <p class="text-sm text-neutral-800">活動付款內容</p> -->
               </div>
 
-              <span class="text-neutral-950 font-semibold">NT$ 300</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-col justify-between px-4 py-4 bg-gray-100 rounded-md md:px-10">
-          <div class="flex flex-row gap-3 md:gap-5">
-            <div class="w-28 h-28 bg-gray-300 rounded-md shrink-0">
-              <img src="" alt="" />
-            </div>
-            <div class="w-2/3 md:w-full flex flex-col justify-between">
-              <div class="">
-                <h3 class="text-neutral-950">活動名稱</h3>
-                <p class="text-sm text-neutral-800">
-                  活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容活動付款內容
-                </p>
-              </div>
-
-              <span class="text-neutral-950 font-semibold">NT$ 300</span>
+              <span class="text-neutral-950 font-semibold">NT$ {{ item.price }}</span>
             </div>
           </div>
         </div>
@@ -52,13 +72,17 @@
             />
           </div> -->
           <div>
-            <div class="flex justify-between text-sm py-1"><span>小計</span> <span>$300</span></div>
-
             <div class="flex justify-between text-sm py-1">
-              <span>已使用折扣碼</span> <span>-$15</span>
+              <span>小計</span>
+              <span>${{ subtotal }}</span>
             </div>
+
+            <!-- <div class="flex justify-between text-sm py-1">
+              <span>已使用折扣碼</span> <span>-$15</span>
+            </div> -->
+
             <div class="flex justify-between text-sm font-semibold py-1">
-              <span>總計</span> <span>$265</span>
+              <span>總計</span> <span>${{ total }}</span>
             </div>
           </div>
           <div>
