725a13a8e136c8a2c4d7e790d49fc957b31b5796
Zhi-Yan,Haung <w0975582420@gmail.com>
2024-12-21 12:42:33 +0800

    feat: render wallet balance and successfully create orders

--- src/apis/checkoutAPI.js
+++ src/apis/checkoutAPI.js
@@ -2,12 +2,17 @@ import { apiAxios } from '@/utils/request.js'
 
 export const getCartItemsAPI = async (uid) => {
   const response = await apiAxios.get(`/carts/${uid}`)
-  console.log('購物車資料獲取成功', response.data)
+  // console.log('購物車資料獲取成功', response.data)
+  return response.data
+}
+
+export const getWalletBalanceAPI = async (uid) => {
+  const response = await apiAxios.get(`/payments/wallet/${uid}`)
   return response.data
 }
 
 export const createOrderAPI = async (orderData) => {
   const response = await apiAxios.post('/orders', orderData)
-  console.log('訂單資料傳送成功', response.data)
-  return response.data
+  // console.log('訂單資料傳送成功', response.data)
+  return response
 }
--- src/views/Payment/CheckoutPage.vue
+++ src/views/Payment/CheckoutPage.vue
@@ -5,6 +5,7 @@ import * as CheckoutAPIs from '../../apis/checkoutAPI.js'
 
 const userStore = useUserStore()
 
+// 抓取購物車明細
 const cartItems = ref([])
 const subtotal = ref(0)
 const fetchCartItems = async () => {
@@ -12,24 +13,63 @@ const fetchCartItems = async () => {
     const response = await CheckoutAPIs.getCartItemsAPI(userStore.user.uid)
 
     cartItems.value = response.data.cartItems.map((item) => ({
+      id: item.activity_id,
       title: item.activities.name,
       photo: item.activities.img_url,
       price: Number(item.activities.price),
     }))
 
+    console.log(cartItems.value)
+
     subtotal.value = cartItems.value.reduce((total, item) => total + item.price, 0)
-    console.log(subtotal.value)
   } catch (error) {
     console.error('購物車資料獲取失敗:', error)
   }
 }
-
+// 計算總金額
 const total = computed(() => {
   return subtotal.value
 })
+// 抓取餘額
+const balance = ref(0)
+const fetchWalletBalance = async () => {
+  try {
+    const response = await CheckoutAPIs.getWalletBalanceAPI(userStore.user.uid)
+    balance.value = response.data.balance
+    console.log('餘額獲取成功', response.data)
+    return balance.value
+  } catch (error) {
+    console.error('餘額獲取失敗:', error)
+  }
+}
+
+// 判斷餘額是否足夠
+const isBalanceEnough = computed(() => {
+  return balance.value >= total.value
+})
+
+// 創建訂單
+const createOrder = async () => {
+  try {
+    const response = await CheckoutAPIs.createOrderAPI({
+      uid: userStore.user.uid,
+      total_amount: total.value,
+      order_status: 'pending',
+      order_item: cartItems.value.map((item) => ({
+        activity_id: item.id,
+        quantity: 1,
+        price: item.price,
+        subtotal: item.price,
+      })),
+    })
+    console.log('訂單創建成功', response.data)
+  } catch (error) {
+    console.error('訂單創建失敗:', error)
+  }
+}
 
 onMounted(() => {
-  fetchCartItems()
+  fetchCartItems(), fetchWalletBalance()
 })
 </script>
 
@@ -87,22 +127,35 @@ onMounted(() => {
           </div>
           <div>
             <div class="flex justify-between text-sm py-1">
-              <span>儲值金餘額</span> <span>$200</span>
+              <span>儲值金餘額</span> <span>${{ balance }}</span>
             </div>
-            <div class="flex justify-between text-sm font-semibold py-1">
-              <span>餘額不足</span> <span>-$65</span>
+            <div v-if="!isBalanceEnough" class="flex justify-between text-sm font-semibold py-1">
+              <span>餘額不足</span> <span>${{ total - balance }}</span>
+            </div>
+            <div v-if="isBalanceEnough" class="flex justify-between text-sm font-semibold py-1">
+              <span>扣除後剩餘儲值金</span> <span>${{ balance - total }}</span>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="rounded-lg text-center text-sm text-red-500 bg-red-100 py-2 px-4">
+      <div
+        v-if="!isBalanceEnough"
+        class="rounded-lg text-center text-sm text-red-500 bg-red-100 py-2 px-4"
+      >
         餘額不足請前往儲值!
       </div>
       <div class="flex justify-between items-center gap-4">
         <button class="flex-1 text-sm text-white bg-gray-500 rounded-md py-2">上一步</button>
-        <button class="flex-1 text-sm text-white bg-red-500 rounded-md py-2">前往儲值頁</button>
-        <button class="flex-1 text-sm text-white bg-gray-500 rounded-md py-2">結帳</button>
+        <button
+          v-if="!isBalanceEnough"
+          class="flex-1 text-sm text-white bg-red-500 rounded-md py-2"
+        >
+          前往儲值頁
+        </button>
+        <button @click="createOrder" class="flex-1 text-sm text-white bg-gray-500 rounded-md py-2">
+          結帳
+        </button>
       </div>
     </div>
   </div>
