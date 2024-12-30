a35c47a4a9cc160011572d96b21e6aebd2f48d43
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-31 00:35:08 +0800

    fix: change the way to get success

--- src/views/TopUp/component/TopupResult.vue
+++ src/views/TopUp/component/TopupResult.vue
@@ -1,21 +1,32 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { getTopupRecordAPI } from '@/apis/topupAPI'
+import { useUserStore } from '@/stores/userStore'
 
 const route = useRoute()
 const router = useRouter()
+const userStore = useUserStore()
+const oneRecord = ref(null)
 
-const getTopupStatus = async () => {
-  const status = route.query.Status
-  // newebpay_transactions的id
+const getOneTopupStatus = async () => {
   const id = route.params.id
-  if (status == 'SUCCESS') {
-    router.push(`/topup/success/${id}`)
-  } else {
-    router.push(`/topup/fail/`)
+  console.log('result page id', id)
+
+  const response = await getTopupRecordAPI(userStore.user.uid)
+  console.log('result page', response)
+
+  if (response) {
+    oneRecord.value = response.find((record) => record.id == id)
+    if (oneRecord.value.payment_status == 'SUCCESS') {
+      router.push(`/topup/success/${id}`)
+    } else {
+      router.push(`/topup/fail/${id}`)
+    }
   }
 }
+
 onMounted(() => {
-  getTopupStatus()
+  getOneTopupStatus()
 })
 </script>
