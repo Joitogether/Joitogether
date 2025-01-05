fd99e6936ed77815e7be566f0f72ce514f154908
yucochann <yuca.work@gmail.com>
2025-01-05 21:58:50 +0800

    style: adjustments completed for all payment-related pages

--- src/views/TopUp/component/WalletRecord.vue
+++ src/views/TopUp/component/WalletRecord.vue
@@ -59,48 +59,49 @@ onMounted(() => {
         <div
           class="text-xl font-bold text-center text-gray-700 tracking-wide absolute left-1/2 -translate-x-1/2"
         >
-          花錢買快樂的紀錄 💸
+          花錢買快樂的紀錄💸
         </div>
       </div>
+      <div class="">
+        <n-table :bordered="false" :single-line="false" class="">
+          <thead>
+            <tr class="text-center text-base">
+              <th>交易日期</th>
+              <th>交易類別</th>
+              <th>交易金額</th>
+              <th class="hidden lg:block">交易序號</th>
+              <th>錢包餘額</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="record in wallet" :key="record.id" class="text-base">
+              <td>{{ formatDate(record.created_at) }}</td>
+              <td v-if="record.action == 'deposit'">儲值金</td>
+              <td v-if="record.action == 'spend'">揪團活動</td>
+              <td v-if="record.action == 'deposit'">
+                <div class="flex justify-between">
+                  <plus class="text-green-500" />
+                  {{ record.amount }}
+                </div>
+              </td>
+              <td v-if="record.action == 'spend'">
+                <div class="flex justify-between">
+                  <minus class="text-red-500" />
+                  {{ record.amount }}
+                </div>
+              </td>
 
-      <n-table :bordered="false" :single-line="false" class="m-auto">
-        <thead>
-          <tr class="text-center text-base">
-            <th>交易日期</th>
-            <th>交易類別</th>
-            <th>交易金額</th>
-            <th class="hidden lg:block">交易序號</th>
-            <th>錢包餘額</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="record in wallet" :key="record.id" class="text-base">
-            <td>{{ formatDate(record.created_at) }}</td>
-            <td v-if="record.action == 'deposit'">儲值金</td>
-            <td v-if="record.action == 'spend'">揪團活動</td>
-            <td v-if="record.action == 'deposit'">
-              <div class="flex justify-between">
-                <plus class="text-green-500" />
-                {{ record.amount }}
-              </div>
-            </td>
-            <td v-if="record.action == 'spend'">
-              <div class="flex justify-between">
-                <minus class="text-red-500" />
-                {{ record.amount }}
-              </div>
-            </td>
-
-            <td class="hidden lg:block">Joimoney{{ record.action }}{{ record.tradeTime }}</td>
-            <td>
-              <div class="flex justify-between">
-                <span>$</span>
-                {{ record.updated_balance }}
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </n-table>
+              <td class="hidden lg:block">Joimoney{{ record.action }}{{ record.tradeTime }}</td>
+              <td>
+                <div class="flex justify-between">
+                  <span>$</span>
+                  {{ record.updated_balance }}
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </n-table>
+      </div>
     </div>
   </div>
 </template>
