e96d35bba773c1ecbf308f565729d68844cdf89e
Junwanghere <change60201@gmail.com>
2025-01-07 16:05:15 +0800

    fix: move loading cat into axios interceptors

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { NConfigProvider, NDialogProvider, NMessageProvider } from 'naive-ui'
 import { getCurrentUser } from './utils/firebaseConfig'
 import catLoading from './views/Home/components/catLoading.vue'
-import { useRouter } from 'vue-router'
-
-const loadingRef = ref(null)
-const router = useRouter()
-
-// 監聽路由變化
-router.beforeEach((to, from, next) => {
-  loadingRef.value?.show()
-  next()
-})
-
-router.afterEach(() => {
-  setTimeout(() => {
-    loadingRef.value?.hide()
-  }, 500) // 適當延遲確保頁面已加載完成
-})
 
 onMounted(async () => {
   await getCurrentUser()
@@ -29,8 +13,9 @@ onMounted(async () => {
   <n-config-provider>
     <n-dialog-provider>
       <n-message-provider>
-        <div id="loading-overlay"></div>
-        <catLoading ref="loadingRef" />
+        <div id="overlay">
+          <catLoading />
+        </div>
         <router-view />
       </n-message-provider>
     </n-dialog-provider>
@@ -38,18 +23,13 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-#loading-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.3);
-  z-index: 5999;
-  display: none;
+#overlay {
+  opacity: 0;
+  pointer-events: none;
 }
 
-#loading-overlay.active {
-  display: block;
+#overlay.active {
+  pointer-events: initial;
+  opacity: 1;
 }
 </style>
