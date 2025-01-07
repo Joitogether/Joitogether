44f9cd629dfd57e5150e28375ba1b0a664d82713
yucochann <yuca.work@gmail.com>
2025-01-07 23:07:02 +0800

    style: change post commenter avatar link

--- src/views/Post/component/PostDetail.vue
+++ src/views/Post/component/PostDetail.vue
@@ -575,7 +575,7 @@ watch(
                   alt="User Avatar"
                   :src="
                     userStore.user.photo_url ||
-                    'https://i.pinimg.com/736x/20/3e/d7/203ed7d8550c2c1c145a2fb24b6fbca3.jpg'
+                    'https://firebasestorage.googleapis.com/v0/b/login-demo1-9d3cb.firebasestorage.app/o/avatars%2Fcatavatar.png?alt=media&token=ccd02591-0c4f-435c-9a4a-34f219774558'
                   "
                   class="w-full h-full bg-yellow-200 object-cover"
                 />
@@ -614,7 +614,7 @@ watch(
                     alt="User Avatar"
                     :src="
                       comment.avatar ||
-                      'https://i.pinimg.com/736x/20/3e/d7/203ed7d8550c2c1c145a2fb24b6fbca3.jpg'
+                      'https://firebasestorage.googleapis.com/v0/b/login-demo1-9d3cb.firebasestorage.app/o/avatars%2Fcatavatar.png?alt=media&token=ccd02591-0c4f-435c-9a4a-34f219774558'
                     "
                     class="w-full h-full bg-yellow-200 object-cover"
                   />
